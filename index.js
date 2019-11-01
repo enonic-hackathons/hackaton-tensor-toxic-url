8e9bb1c549a02abacc7a9ea6dd6c9965a267a193
Christian Westgaard <cwe@enonic.com>
2019-11-01 11:33:42 +0100

    Detect sentences

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const tf = require('@tensorflow/tfjs-node-gpu');
 const toxicity = require('@tensorflow-models/toxicity');
 const {print} = require('q-i');
 const {default: surgeon} = require('surgeon');
+const {sentences: findSentences} = require('sbd');
+const setIn = require('set-value');
+//const strShorten = require('str_shorten')
 //print(surgeon);
 //const {cheerioEvaluator} = require('surgeon/dist/evaluators');
 
@@ -10,9 +13,14 @@ const x = surgeon(/*{
   evaluator: cheerioEvaluator()
 }*/);
 
+//const LENGTH = 100;
+
+const WHITELIST_PATTERN = `[^-a-zA-Z_ '''"ÆÐƎƏƐƔĲŊŒẞÞǷȜæðǝəɛɣĳŋœĸſßþƿȝĄƁÇĐƊĘĦĮƘŁØƠŞȘŢȚŦŲƯY̨Ƴąɓçđɗęħįƙłøơşșţțŧųưy̨ƴÁÀÂÄǍĂĀÃÅǺĄÆǼǢƁĆĊĈČÇĎḌĐƊÐÉÈĖÊËĚĔĒĘẸƎƏƐĠĜǦĞĢƔáàâäǎăāãåǻąæǽǣɓćċĉčçďḍđɗðéèėêëěĕēęẹǝəɛġĝǧğģɣĤḤĦIÍÌİÎÏǏĬĪĨĮỊĲĴĶƘĹĻŁĽĿʼNŃN̈ŇÑŅŊÓÒÔÖǑŎŌÕŐỌØǾƠŒĥḥħıíìiîïǐĭīĩįịĳĵķƙĸĺļłľŀŉńn̈ňñņŋóòôöǒŏōõőọøǿơœŔŘŖŚŜŠŞȘṢẞŤŢṬŦÞÚÙÛÜǓŬŪŨŰŮŲỤƯẂẀŴẄǷÝỲŶŸȲỸƳŹŻŽẒŕřŗſśŝšşșṣßťţṭŧþúùûüǔŭūũűůųụưẃẁŵẅƿýỳŷÿȳỹƴźżžẓ]+`;
+const WHITELIST_REGEXP = new RegExp(WHITELIST_PATTERN, 'gi');
+
 const THRESHOLD = 0.9;
 
-const URL = process.argv[2];//print(URL);
+const URL = process.argv[2]; print(URL);
 
 superagent.get(URL).end((err, res) => {
   //print(err);
@@ -21,16 +29,26 @@ superagent.get(URL).end((err, res) => {
   const text = x(`select html | remove img {0,} | read property textContent`, res.text)
     .replace(/\n/g,'')
     .replace(/\s+/g, ' ').trim();
-  print(text);
+
+  const sentences = findSentences(text, {}).map(str => str.replace(WHITELIST_REGEXP, ''));
+  //print(sentences);
+
   toxicity.load(THRESHOLD).then(model => {
-    const sentences = [res.text];
     model.classify(sentences).then(predictions => {
-      const h = {};
+      const s = {};
+      //print(predictions);
       predictions.forEach(({label, results}) => {
-        const [{match}] = results;
-        h[label] = match;
-      })
-      print({URL, toxicity:h});
+        results.forEach((result, i) => {
+          const sentence = sentences[i];
+          const {match} = result;
+          //setIn(s, `${sentence}.${label}`, result);
+          if(match) {
+            setIn(s, `${sentence}.${label}`, match);
+          }
+        });
+      });
+      print(s);
     });
   });
+
 });
