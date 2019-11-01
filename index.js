eaf02da68403fa5191e66d729d362e66967df398
Christian Westgaard <cwe@enonic.com>
2019-11-01 10:19:20 +0100

    Added surgeon

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+const superagent = require('superagent');
 const tf = require('@tensorflow/tfjs-node-gpu');
 const toxicity = require('@tensorflow-models/toxicity');
 const {print} = require('q-i');
+const {default: surgeon} = require('surgeon');
+//print(surgeon);
+//const {cheerioEvaluator} = require('surgeon/dist/evaluators');
+
+const x = surgeon(/*{
+  evaluator: cheerioEvaluator()
+}*/);
+
 const THRESHOLD = 0.9;
-toxicity.load(THRESHOLD).then(model => {
-  const sentences = ['you suck'];
-  model.classify(sentences).then(predictions => {
-    const h = {};
-    predictions.forEach(({label, results}) => {
-      const [{match}] = results;
-      h[label] = match;
-    })
-    print(h);
+
+const URL = process.argv[2];//print(URL);
+
+superagent.get(URL).end((err, res) => {
+  //print(err);
+  //print(res);
+  //print(res.text);
+  const text = x('select html | read property textContent', res.text)
+    .replace(/\n/g,'')
+    .replace(/\s+/g, ' ').trim();
+  //print(text);
+  toxicity.load(THRESHOLD).then(model => {
+    const sentences = [res.text];
+    model.classify(sentences).then(predictions => {
+      const h = {};
+      predictions.forEach(({label, results}) => {
+        const [{match}] = results;
+        h[label] = match;
+      })
+      print({URL, toxicity:h});
+    });
   });
 });
